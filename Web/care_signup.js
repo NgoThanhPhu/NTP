2ff4e834c4434bc7d1cb5fa4e48263586557e2f2
Ngô Thanh Phú <45889218+NgoThanhPhu@users.noreply.github.com>
2020-03-04 15:13:13 +0700

    Add files via upload

---
+++ Web/care_signup.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#arrow").click(function () {
+        $(".main__dangky__form2").slideToggle(0);
+    })
+})
+
+$(document).ready(function () {
+    $("#arrow").click(function () {
+        $(".main__dangky__form3").toggle();
+    })
+})
+
+
